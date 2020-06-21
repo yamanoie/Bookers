5f43673ee9a8a783b270d3b3a2b375c02c1d3e57
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-06-21 05:57:31 +0000

    all function finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
-  	@books = Book.all
+  	@books = Book.all.order(id: :desc)
+  	@book = Book.new
   end
 
   def show
@@ -12,9 +13,14 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Blog.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	@book = Book.new(book_params)
+  	if @book.save
+  		flash[:notice] = "successfully"
+  		redirect_to book_path(@book.id)
+  	else
+  		@books = Book.all.order(id: :desc)
+  		render :index
+  	end
   end
 
   def edit
@@ -22,19 +28,24 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(parmas)
-  	blog.update(blog_params)
-  	redirect_to book_path(book)
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		flash[:notice] = "successfully"
+  		redirect_to book_path(@book.id)
+  	else
+  		render :edit
+  	end
   end
 
   def destroy
-  	book = Blog.find(params[:id])
-  	blog.destroy
+  	book = Book.find(params[:id])
+  	book.destroy
   	redirect_to books_path
+  end
 
   private
-  def book_params
-  	params.require(:book).permit(:title, :body)
-  end
+  	def book_params
+  		params.require(:book).permit(:title, :body)
+  	end
 
 end
