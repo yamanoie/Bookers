2aa2fb97ad5de8209668a126aaa79cde8e98c35a
Yuki Yamamoto <yamanoieyamanoie@yahoo.co.jp>
2020-06-21 02:10:17 +0000

    new finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,21 @@ class BooksController < ApplicationController
   end
 
   def new
+  	@book = Book.new
+  end
+
+  def create
+  	book = Blog.new(book_params)
+  	book.save
+  	redirect_to books_path
   end
 
   def edit
   end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
 end
